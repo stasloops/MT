f81e8c62b7ad70dccabb17731a56c8032c759093
nech <sowul789@gmail.com>
2024-06-30 01:54:59 +0300

    wtfffff

--- src/server/auth/controller.ts
+++ src/server/auth/controller.ts
@@ -1,21 +1,5 @@
-import jwt from "jsonwebtoken";
 import { NextResponse } from "next/server";
-import { AuthDataValidator } from "@telegram-auth/server";
-import { urlStrToAuthDataMap } from "@telegram-auth/server/utils";
 
 export const POST = async (req: Request, res: Response) => {
-  NextResponse.json({ user: 'finally' }, { status: 200 });
-};
-
-export const GET = async (req: Request, res: Response) => {
-  try {
-    const token = req.headers.get("token");
-    const is = jwt.verify(token || "", process.env.SHA || "");
-    return NextResponse.json({ is: is });
-  } catch (err: any) {
-    return NextResponse.json(
-      { err: err },
-      { status: 401, statusText: err.message }
-    );
-  }
+  NextResponse.json({ user: "finally" }, { status: 200 });
 };
--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -1,1 +1,1 @@
-export { GET, POST } from "./controller";
+export { POST } from "./controller";
