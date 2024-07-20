f6b24c36a5e9026cae40a3f5d8c6f163c269a1e4
nech <sowul789@gmail.com>
2024-07-20 10:42:23 +0300

    dssd

--- src/shared/ui/design_system/bottom_menu/ui/bottom_menu.tsx
+++ src/shared/ui/design_system/bottom_menu/ui/bottom_menu.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import styles from "./bottom_menu.module.scss";
 import clsx from "clsx";
 
@@ -9,30 +9,51 @@ interface Props {
 }
 
 export const BottomMenu: FC<Props> = ({ items }) => {
-  const [activeTab, setActiveTab] = useState(items[0]?.label);
+  const [activeBlockPercentWidth, setActiveBlockPercentWidth] =
+    useState<number>(0);
+  const [activeTab, setActiveTab] = useState(0);
 
-  const handleClick = (label: string) => {
-    setActiveTab(label);
+  const handleClick = (activeTab: number) => {
+    setActiveTab(activeTab);
   };
+
+  useEffect(() => {
+    setActiveBlockPercentWidth((window.innerWidth / items.length / 1.2) * 2);
+  }, []);
+
   return (
     <div className={styles.wrapper}>
-      {items.map((item) => {
+      <div
+        className={styles.background}
+        style={{
+          width: `${activeBlockPercentWidth}px`,
+          transform: `translateX(${50 * activeTab}%)`,
+        }}
+      ></div>
+
+      {items.map((item, index) => {
         const Icon = item.icon;
-        const isActive = activeTab === item.label;
+        const isActive = activeTab === index;
         return (
           <div
-            onClick={() => handleClick(item.label)}
+            onClick={() => handleClick(index)}
             className={clsx(styles.item, isActive && styles.item_active)}
             key={item.label}
           >
+            <div className={clsx(styles.item_border, styles.item_border_left)}></div>
             <Icon
               className={clsx(styles.icon, isActive && styles.icon_active)}
               width={item.width}
               height={item.height}
             />
-            <div className={clsx(styles.label, isActive && styles.label_active)}>
-              {item.label}
+            <div className={styles.item_label}>
+              <div
+                className={clsx(styles.label, isActive && styles.label_active)}
+              >
+                {item.label}
+              </div>
             </div>
+            <div className={clsx(styles.item_border, styles.item_border_right)}></div>
           </div>
         );
       })}
