91a1bf997f62c7d30e4e1de95c9b5289b5f08344
nech <sowul789@gmail.com>
2024-07-27 22:17:51 +0300

    wead

--- src/shared/lib/utils/vibrateDevice.ts
+++ src/shared/lib/utils/vibrateDevice.ts
@@ -1,5 +1,8 @@
 export const vibrateDevice = (duration: number) => {
   if ("vibrate" in navigator) {
     navigator.vibrate(duration);
+    alert("Vibrate supported");
+  } else {
+    alert("Vibrate not supported");
   }
 };
