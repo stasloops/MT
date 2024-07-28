109448e3768636068cc9ea10d6959864feae8619
nech <sowul789@gmail.com>
2024-07-28 22:34:13 +0300

    dfadsf

--- src/shared/lib/router/ui/router.tsx
+++ src/shared/lib/router/ui/router.tsx
@@ -1,8 +1,9 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import styles from "./router.module.scss";
 import { useUnit } from "effector-react";
 import clsx from "clsx";
 import { bottom_menu_model } from "@/shared/ui/design_system/bottom_menu";
+import { animated, useSpring } from "@react-spring/web";
 
 interface Props {
   children: React.ReactNode[];
@@ -10,14 +11,22 @@ interface Props {
 
 export const Router: FC<Props> = ({ children }) => {
   const [activeTab] = useUnit([bottom_menu_model.$activeTab]);
-  
+  const [springs, api] = useSpring(() => {
+    return {
+      from: { transform: `translateX(-${activeTab * 100}vw)` },
+    };
+  });
+
+  useEffect(() => {
+    api.start({
+      to: { transform: `translateX(-${activeTab * 100}vw)` },
+     
+    });
+  }, [activeTab]);
 
   return (
-    <div
-      className={clsx(styles.pages)}
-      style={{ transform: `translateX(-${activeTab * 100}vw)` }}
-    >
+    <animated.div className={clsx(styles.pages)} style={{ ...springs }}>
       {children}
-    </div>
+    </animated.div>
   );
 };
